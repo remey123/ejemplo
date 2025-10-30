390fd7ba1ad6fd603766ef787088e4395b976f78
MEILLON MONTIEL <260289@alumnos.uia>
2025-10-30 10:13:20 -0600

    primera versi{on de programa que calcula promedios

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+try:
+    archivo = open("texto-guardado.txt", "w")
+    archivo.write("usé python para escribir un archivo")
+except:
+    print("No pude abrir el archivo")
+finally: 
+    archivo.close()
